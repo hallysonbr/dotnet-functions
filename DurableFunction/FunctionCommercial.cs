b9a8dc3aac3b0b02b8ee818cd7dae0c56379a217
hbmelo <hbmelo@stefanini.com>
2024-05-28 21:19:51 -0300

    Criando uma Durable Function

---
+++ DurableFunction/FunctionCommercial.cs
@@ -0,0 +1,21 @@
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.DurableTask;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Linq;
+
+namespace DurableFunction
+{
+    public class FunctionCommercial
+    {
+        [FunctionName(nameof(FunctionCommercial))]
+        public Task<string> Run([ActivityTrigger] string name, ILogger log)
+        {
+            log.LogInformation("Saying hello to {name}.", name);
+            return Task.FromResult($"Hello {name}!");
+        }
+    }
+}
